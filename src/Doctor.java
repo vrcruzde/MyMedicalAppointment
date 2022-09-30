61ac7086274bfb5ba13064adc0f85dd85a40e245
vrcruzde <virocde@hotmail.com>
2022-09-30 07:48:01 -0500

    Se crea clase User, haciendo herencia con Patient y Doctor

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,33 +1,16 @@
 import java.util.ArrayList;
 import java.util.Date;
 
-public class Doctor {
+public class Doctor extends User {
     //Atributos
-    static int id = 0;  // debe ser autoincrementado por cada Doctor aumenta su valor y el primer valor cera O
-    private String name;
-    private String email;
     private  String speciality;
-
-
 
     // Comportamientos
 
-    Doctor (){
-        System.out.println("Contruyendo el objeto Doctor");
-
-    }
-    Doctor (String name, String speciality){
+      Doctor (String name, String email){
+        super(name, email);
         System.out.println("El nombre del doctor asignado es"+ name);
-        id++;
-        this.name = name;// Hace referencia a todos los elementos que componen esa clase
-        this.speciality = speciality;
-    }
-    public void showName(){
-        System.out.println(name);
-    }
 
-    public void showIde(){
-        System.out.println("ID Doctor:  "+id);
     }
 
     //Coleccion de objetos
--- src/Patient.java
+++ src/Patient.java
@@ -1,10 +1,5 @@
-public class Patient {
+public class Patient extends User{
     // atributos
-    int id;
-    private String name;
-    private String email;
-    private String address;
-    private String phoneNumber;
     private String birthday;
     private double weight;
     private double height;
@@ -12,48 +7,12 @@ public class Patient {
 
     // constructor, se encarga de definir los parametros obligatorios
     //para que el objeto exista
-    Patient (String name, String email) {
-        this.name = name;
-        this.email = email;
-    }
+     Patient(String name, String email){
+         super(name,email);
+     }
+
     //METODOS GET Y SET
     //me validan todo dato que se quiera asignar
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        if (phoneNumber.length()>8){
-            System.out.println("El numero telefonico debe tener 8 caracteres maximo");
-        }else if (phoneNumber.length() == 8){
-            this.phoneNumber = phoneNumber;
-         }
-    }
-
     public String getBirthday() {
         return birthday;
     }
