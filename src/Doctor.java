6271f7d4a87f685058e02050374263140812b513
vrcruzde <virocde@hotmail.com>
2022-09-30 05:34:25 -0500

    Se crea clase anidada AvailableAppointment y Array para fechas y hora de citas disponibles

--- src/Doctor.java
+++ src/Doctor.java
@@ -1,9 +1,14 @@
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Doctor {
     //Atributos
     static int id = 0;  // debe ser autoincrementado por cada Doctor aumenta su valor y el primer valor cera O
-    String name;
-    String email;
-    String speciality;
+    private String name;
+    private String email;
+    private  String speciality;
+
+
 
     // Comportamientos
 
@@ -23,5 +28,52 @@ public class Doctor {
 
     public void showIde(){
         System.out.println("ID Doctor:  "+id);
+    }
+
+    //Coleccion de objetos
+    //Cada vez que se agrege un nueva cita se estara acumulado en el array
+    ArrayList<AvailableAppointment>availableAppointments = new ArrayList<>();
+    public void addAvailableAppointment(Date date, String time){ //metodo para disponibilidad de citas
+        availableAppointments.add(new Doctor.AvailableAppointment(date, time));
+    }
+
+    //nos ayuda a devolver la lista completa de citas
+    public ArrayList<AvailableAppointment>getAvailableAppointments(){
+        return availableAppointments;
+    }
+    public static class AvailableAppointment{
+        //Doctor define fechas en las que esta disponible
+        private int id;
+        private Date date; //fecha
+        private String time;// hora
+
+        public AvailableAppointment(Date date,String time){
+            this.date = date;
+            this.time = time;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public Date getDate() {
+            return date;
+        }
+
+        public void setDate(Date date) {
+            this.date = date;
+        }
+
+        public String getTime() {
+            return time;
+        }
+
+        public void setTime(String time) {
+            this.time = time;
+        }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Date;
+
 import static ui.UIMenu.*;
 
 public class Main {
@@ -6,15 +8,20 @@ public class Main {
         //showMenu();
 
         Doctor myDoctor = new Doctor("Victor Cruz","Cardiology");
-        System.out.println(myDoctor.name);
-        System.out.println(myDoctor.speciality);
+        myDoctor.addAvailableAppointment(new Date(), "4:30pm");
+        myDoctor.addAvailableAppointment(new Date(),"5pm");
+        myDoctor.addAvailableAppointment(new Date(),"5:30pm");
 
-        Patient myPatient = new Patient("Alejandro Cruz","acruz@desarrollo.com");
+        for (Doctor.AvailableAppointment aA: myDoctor.getAvailableAppointments()) {
+            System.out.println(aA.getDate()+""+aA.getTime());
+        }
+
+       /* Patient myPatient = new Patient("Alejandro Cruz","acruz@desarrollo.com");
         myPatient.setWeight(72.3);
         System.out.println(myPatient.getWeight());
 
         myPatient.setPhoneNumber("12354697");
-        System.out.println(myPatient.getPhoneNumber());
+        System.out.println(myPatient.getPhoneNumber());*/
     }
 
 
