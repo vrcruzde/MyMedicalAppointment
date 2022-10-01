420fad0e6096e81ba1204d457b2c5031e6a0a594
vrcruzde <virocde@hotmail.com>
2022-10-01 14:36:14 -0500

    Se simula que se traen datos de una DB,con arrayList y switchear hacia que menu queremos seleccionar Doctor o paciente

--- src/ui/UIMenu.java
+++ src/ui/UIMenu.java
@@ -1,19 +1,26 @@
 package ui;
 
+import model.Doctor;
+import model.Patient;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class UIMenu {
 
     public static String[] MONTHS = {
             "Enero","Febrero","Marzo","Abril","Mayo","Junio","Julio","Agosto","Septiembre","Octubre","Noviembre","Diciembre"};
+    public static Doctor doctorLogged;
+    public static Patient patientLogged;
+
     public static void showMenu(){
         System.out.println("Welcome to My Appointments");
         System.out.println("Selecciona la opcion");
 
         int response =0;
         do{
-            System.out.println("1. model.Doctor");
-            System.out.println("2. model.Patient");
+            System.out.println("1. Doctor");
+            System.out.println("2. Patient");
             System.out.println("0. Salir");
 
             Scanner sc = new Scanner(System.in);
@@ -21,11 +28,13 @@ public class UIMenu {
 
             switch (response){
                 case 1:
-                    System.out.println("model.Doctor");
+                    System.out.println("Doctor");
+                    response = 0; // para que el ciclo de vida del bucle se complete
+                    authUser(1);
                     break;
                 case 2:
                     response = 0;
-                    showPatientMenu();
+                    authUser(2);
                 case 0:
                     System.out.println("Thanks you for you visit");
                     break;
@@ -33,6 +42,55 @@ public class UIMenu {
                     System.out.println("Please select a correct answer");
             }
         }while (response!=0);
+    }
+    //metodo que simula autenticacion, que define parametro para
+    //saber que tipo es seria int
+    private static void authUser(int userType){
+        //userType=1 Doctor
+        //userType=2 paciente
+        //se guarda lista y llenar objetos en arraylist
+
+        ArrayList<Doctor>doctors=new ArrayList<>();
+        doctors.add(new Doctor("Alejandro Cruz", "alc@hos.com"));
+        doctors.add(new Doctor("Victor Cruz","vcr@cle.com"));
+        doctors.add(new Doctor("Ariana Cruz","arc@cle.com"));
+
+        ArrayList<Patient>patients=new ArrayList<>();
+        patients.add(new Patient("Deisy Imbachi", "dei@gmail.com"));
+        patients.add(new Patient("Carlos Ponse","cpon@hotmail.com"));
+        patients.add(new Patient("Julian Roman","jr@gmaik.com"));
+
+        boolean emailCorrect=false;// esta en false porque hasta el momento que haga la verificacion pasa a true
+        do {
+            System.out.println("Insert your email Ej: a@a.com");
+            Scanner sc= new Scanner(System.in);// para recibir los datos digitados
+            String email = sc.nextLine(); // para recibir los datos digitados en variable email
+
+            if (userType==1){
+                for (Doctor d:doctors){
+                    if (d.getEmail().equals(email)){
+                        emailCorrect=true;
+                        //obtener el usuario logeado
+                        doctorLogged=d;
+                        // showDoctorMenu
+                    }
+                }
+            }
+            if (userType==2){
+                for (Patient p:patients){
+                    if (p.getEmail().equals(email)){
+                        emailCorrect=true;
+                        patientLogged=p;
+                        //showPatientMenu
+                    }
+                }
+
+
+            }
+
+
+        }while (!emailCorrect);
+
     }
 
     public static void showPatientMenu(){
